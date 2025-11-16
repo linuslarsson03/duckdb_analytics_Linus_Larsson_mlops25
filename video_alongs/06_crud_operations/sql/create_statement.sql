6fcf68ac641758d6aca0188564e45350d612c3d9
linus larsson <linus.anders.larsson@gmail.com>
2025-11-16 19:00:48 +0100

    working in 1_crud_create.sql

--- video_alongs/06_crud_operations/sql/create_statement.sql
+++ video_alongs/06_crud_operations/sql/create_statement.sql
@@ -13,3 +13,19 @@ CREATE SEQUENCE IF NOT EXISTS id_duckdb_sequence START 1;
 
 
 FROM pg_catalog.pg_sequences;
+
+-- tables for each glossary table 
+CREATE TABLE IF NOT EXISTS database.sql (
+	id INTEGER DEFAULT nextval('id_sql_sequence'),
+	word STRING,
+	description STRING);
+
+CREATE TABLE IF NOT EXISTS database.duckdb (
+	id INTEGER DEFAULT nextval('id_duckdb_sequence'),
+	word STRING,
+	description STRING);
+
+CREATE TABLE IF NOT EXISTS programming.python (
+	id INTEGER DEFAULT nextval('id_python_sequence'),
+	word STRING,
+	description STRING);
