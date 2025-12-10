65934c8d02d9c1c5045f63be98930a7b1823f6e6
linus larsson <linus.anders.larsson@gmail.com>
2025-12-10 19:20:57 +0100

    set operations

--- video_alongs/11_set_operations/sql/set_operation_sakila.sql
+++ video_alongs/11_set_operations/sql/set_operation_sakila.sql
@@ -1,3 +1,5 @@
+-- Select all customers and all actors starting thier first name with the letter a.
+
 SELECT
     'customer' AS TYPE,
     c.first_name,
@@ -5,3 +7,38 @@ SELECT
 FROM customer c
 WHERE
     c.first_name LIKE 'A%'
+UNION
+SELECT
+    'Actor',
+    a.first_name,
+    a.last_name,
+FROM Actor a
+WHERE a.first_name LIKE 'A%'
+ORDER BY first_name;
+
+-- Find overlapping names between actors and customers
+SELECT
+    a.first_name,
+    a.last_name
+FROM actor a
+INTERSECT 
+SELECT
+    c.first_name,
+    c.last_name
+FROM customer c;
+
+-- find all with intial JD and record its type (actor, customer)
+
+SELECT
+    'actor' AS "type",
+    a.first_name,
+    a.last_name
+FROM actor a
+WHERE a.first_name LIKE 'J%' AND a.last_name LIKE 'D%'
+UNION ALL
+SELECT
+    'customer' AS "type",
+    c.first_name,
+    c.last_name
+FROM customer c
+WHERE c.first_name LIKE 'J%' AND c.last_name LIKE 'D%'
